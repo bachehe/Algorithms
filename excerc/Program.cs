417462712090a84e99e1b4f04d5142d7173ce266
Damian Bachowski <bachowski.d@e80group.com>
2023-05-22 16:45:47 +0200

    d

--- excerc/Program.cs
+++ excerc/Program.cs
@@ -48,6 +48,7 @@ namespace exerc
             TwoSum(temp, 4);
 
             LengthOfLongestSubstring("abcabcbb");
+
         }
         #region done
 
@@ -434,9 +435,13 @@ namespace exerc
             }
             return count;
         }
-        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2, int carry = 0)
         {
+            if (l1 == null && l2 == null && carry == 0) return null;
 
+            int total = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carry;
+            carry = total / 10;
+            return new ListNode(total % 10, AddTwoNumbers(l1?.next, l2?.next, carry));
         }
     }
     public class ListNode
