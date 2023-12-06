358c33983a509be26d93d2e8dc1f622d56d1d59b
Damian Bachowski <bachowski.d@e80group.com>
2023-12-06 08:40:39 +0100

    update

---
+++ excerc/Exerc/Invoke.cs
@@ -0,0 +1,117 @@
+﻿namespace exerc
+{
+    public class Invoke : Exercises
+    {
+        public void Invoker()
+        {
+            #region done
+            //string key = "tg";
+            //string value = "dop";
+            //int timestamp = 0;
+
+            //var obj = new TimeMap();
+            //obj.Set(key, value, timestamp);
+            //var p2 = obj.Get(key, timestamp);
+            //Console.WriteLine(p2);
+
+            //var n1 = new int[] { 1, 2 };
+            //var n2 = new int[] { 3, 4 };
+            //FindMedianSortedArrays(n1, n2);
+            //var piles = new int[] { 30, 11, 23, 4, 20 };
+            //MinEatingSpeed(piles, 5);
+
+            //var nums = new int[] { 3, 4, 1, 2 };
+            //FindMin(nums);
+
+            //var nums = new int[] { 5, 1, 3 };
+            //Console.WriteLine(Searching(nums, 5));
+
+            //var nums = new int[] { -1, 0, 3, 5, 9, 12 };
+            //var target = 0;
+            //Console.WriteLine(Search(nums, target));
+
+            //var matrix = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 } };
+            //var target = 3;
+            //Console.WriteLine(SearchMatrix(matrix, target));
+
+            //var nums = new int[] { 2, 4 };
+            //LargestRecangle(nums);
+            //var nums = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+            //Trap(nums);
+            //var heights = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            //MaxArea(heights);
+            //var n = new int[] { 73, 74, 75, 71, 69, 72, 76, 73 };
+            //DailyTemperatures(n);
+
+            //var target = 100;
+            //var pos = new int[] { 10, 8, 0, 5, 3 };
+            //var speed = new int[] { 2, 4, 1, 1, 3 };
+            //CarFleet(target, pos, speed);
+
+            //var h = new int[] { 2, 1, 5, 6, 2, 3 };
+
+            //LargestRectangleArea(h);
+
+            //var nums = new int[] { -1, 0, -1, -1, 0, -1, -1, -1, 2 };
+            //ThreeSum(nums);
+            //int n = 1;
+            //GenerateParenthesis(n);
+            //OrderByDescInt();
+            //Console.WriteLine(Underscore());
+
+            //int[] exampleTest1 = { 5, 5, 5, 4 };
+            //Console.WriteLine(Int(exampleTest1));
+
+            //string roman = "MMCVIX";
+            //Console.WriteLine(RomanCalculator(roman));
+
+            //var names = new[] { "Peter", "huj", "fuj", "sruj" };
+            //Console.WriteLine(StringMethod(names));
+
+            //long nb = 4820639;
+            //Console.WriteLine(ExpandedForm(nb));
+
+            //int startPriceOld = 2000;
+            //int startPriceNew = 8000;
+            //int savingPerMonth = 1000;
+            //double percentLossByMonth = 1.5;
+            //Console.WriteLine(CarRentalCalculator(startPriceOld, startPriceNew, savingPerMonth, percentLossByMonth));
+
+            //string nb = "7842126960";
+            //Console.WriteLine(ValidPhoneNumber(nb));
+
+            //string[] smile = new[] { ":)", ":D", ":(" };
+            //CountSimleys(smile);
+
+            //int[,] board = new int[,] { { 1, 2, 2 }, { 0, 2, 0 }, { 0, 2, 0 } };
+            //Console.WriteLine(TicTacToe(board));
+
+            //string s = "mischtschenkoana";
+            //Console.WriteLine(SolveForHighestNumber(s));
+            //var temp = new int[] { 1, 2, 3 };
+            //TwoSum(temp, 4);
+
+            //LengthOfLongestSubstring("abcabcbb");
+            //var nums = new int[] { 100, 4, 200, 1, 3, 2 };
+            //LongestConsecutive(nums);
+
+            //var s = "0P";
+            //Console.WriteLine(IsPalindrome(s));
+
+            //var s = new int[] { 5, 25, 75 };
+            //TwoSums(s, 100);
+
+            //var s = new Stack<string>();
+            //int val = 3;
+            //MinStack obj = new MinStack();
+            //obj.Push(val);
+            //obj.Pop();
+            //int param_3 = obj.Top();
+            //int param_4 = obj.GetMin();
+            //var s = new string[] { "4", "13", "5", "/", "+" };
+            //EvalRPN(s);
+            #endregion
+            Bubble();
+        }
+    }
+}
---
+++ excerc/Exerc/MinStack.cs
@@ -0,0 +1,44 @@
+﻿namespace exerc
+{
+    public class MinStack
+    {
+        Stack<int> stack = null;
+        Stack<int> minStack = null;
+        public MinStack()
+        {
+            stack = new Stack<int>();
+            minStack = new Stack<int>();
+        }
+
+        public void Push(int val)
+        {
+            if (minStack.Count <= 0)
+                minStack.Push(val);
+
+
+            if (minStack.Peek() >= val)
+                minStack.Push(val);
+
+            stack.Push(val);
+        }
+
+        public void Pop()
+        {
+            int val = 0;
+            val = stack.Pop();
+
+            if (minStack.Count > 0 && val == minStack.Peek())
+                minStack.Pop();
+        }
+
+        public int Top()
+        {
+            return stack.Peek();
+        }
+
+        public int GetMin()
+        {
+            return minStack.Peek();
+        }
+    }
+}
