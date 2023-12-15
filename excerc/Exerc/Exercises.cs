c9208869f80cee1f28f052d3df63b80735a1843a
Damian Bachowski <bachowski.d@e80group.com>
2023-12-15 13:30:04 +0100

    update

--- excerc/Exerc/Exercises.cs
+++ excerc/Exerc/Exercises.cs
@@ -50,6 +50,15 @@ namespace exerc
 
             return node;
         }
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2, int carry = 0)
+        {
+            if (l1 == null && l2 == null && carry == 0) return null;
+
+            int total = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carry;
+            carry = total / 10;
+            return new ListNode(total % 10, AddTwoNumbers(l1?.next, l2?.next, carry));
+        }
+
         #endregion nodes
 
         #region done
@@ -933,14 +942,6 @@ namespace exerc
                 }
             }
             return count;
-        }
-        public ListNode AddTwoNumbers(ListNode l1, ListNode l2, int carry = 0)
-        {
-            if (l1 == null && l2 == null && carry == 0) return null;
-
-            int total = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carry;
-            carry = total / 10;
-            return new ListNode(total % 10, AddTwoNumbers(l1?.next, l2?.next, carry));
         }
         #endregion
     }
