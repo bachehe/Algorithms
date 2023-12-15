8249bd12e5ee018e73ffd1168c02242eb93be360
Damian Bachowski <bachowski.d@e80group.com>
2023-12-15 13:29:20 +0100

    update

--- excerc/Exerc/Exercises.cs
+++ excerc/Exerc/Exercises.cs
@@ -9,6 +9,35 @@ namespace exerc
         static List<string> resParenthesis = new List<string>();
 
         #region nodes
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode list = new ListNode();
+            ListNode current = list;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val <= list2.val)
+                {
+                    current.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    current.next = list2;
+                    list2 = list2.next;
+                }
+                current = current.next;
+            }
+
+            if (list1 != null)
+                current.next = list1;
+
+            else
+                current.next = list2;
+
+
+            return list.next;
+        }
         public static ListNode ReverseList(ListNode head)
         {
             ListNode node = null;
